1e75e2e6eced52c6e68e9003d83a222ab4bd29e0
Matthias Kammüller <matthias@kammueller.eu>
2018-10-05 10:04:34 -0400

    polished add-container:
    - basic JS to show and hide it
    - added labels

---
+++ script.js
@@ -0,0 +1,12 @@
+function showAdd() {
+    document.getElementById("add-container").style.display = "block";
+}
+
+function hideAdd() {
+    document.getElementById("add-container").style.display = "none";
+}
+
+function addValue() {
+    // TODO: add value...
+    hideAdd();
+}
