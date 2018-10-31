3ef9015ed8fb8b4bd477114fb9bab9e5ba01ba37
Matthias Kammüller <matthias@kammueller.eu>
2018-10-30 20:02:58 -0400

    using new JS

--- script.js
+++ script.js
@@ -7,16 +7,15 @@ function hideAdd() {
 }
 
 function toggleAdd() {
-  alert("deprecated")
+  alert("deprecated");
 }
 
 function addPref() {
-  document.getElementById('new-name').setCustomValidity("Name of Preference must be unique!");
+  document.getElementById("new-name").setCustomValidity("Name of Preference must be unique!");
 }
 
-function editPref() {
-  alert("should be saved...");
-  // TODO: prevent reload of page?!
+function dummy() {
+  alert("some action!");
 }
 
 function ShowPrefs() {
@@ -34,81 +33,177 @@ function ShowPrefs() {
  * @param isUser true if user-created, i.e. no default-value (has to be modified then)
  */
 function addItem(name, value, isLocked, isModified, Type, isFav, isUser) {
-  // TODO: codestyle!!
-  document.getElementById("pref-table").innerHTML +=
-    '<ul class="pref-table__row' + (isModified ? ' pref-table__row--edited' : '') +
-    '" aria-label="Preference ' + name + '" role="list">\n' +
-    '<li class="pref-table__cell">\n' +
-    (isLocked
-      ? '<button class="button--ghost icon icon--lock" title="locked" disabled></button>\n'
-      : (isFav
-          ? '<button class="button--ghost icon icon--filled-star" title="is favourite"></button>\n'
-          : '<button class="button--ghost icon icon--star" title="make favourite"></button>\n'
-      )) +
-    '</li>\n' +
-    '<li class="pref-table__cell">\n' +
-    '<span' + (isLocked ? ' class="pref-table__cell__disabled-text"' : '') +
-    '>' + name.split(".").join(".<wbr>") + '</span>\n' +
-    '</li>\n' +
-    (Type === "bool"
-        ? '<li class="pref-table__cell"><span' +
-        (isLocked ? ' class="pref-table__cell__disabled-text"' : '') + '\n>' + value + '</span></li>'
+  var type = "String";
+  if (Type === "bool") {
+    type = "Boolean";
+  }
+  if (Type === "int") {
+    type = "Number";
+  }
+  var formName = "form-" + name;
 
-        : '<li class="pref-table__cell">\n' + (isLocked
-        ? '<span class="pref-table__cell__disabled-text">' + value + '</span>\n'
-        : '<form id="edit-form-' + name + '" aria-label="Edit Value" onsubmit="editPref()">\n' +
-        '<input type="text" value="' + value + '"' +
-        (Type === "int"
-            ? 'pattern="[0-9]*" title="Please enter an integer value">\n'
-            : 'title="Please enter the value">\n'
-        ) +
-        '</form>\n') +
-        '</li>\n'
-    ) +
-    '<li class="pref-table__cell">\n' +
-    '<button class="button--small" form="edit-form-' + name + '"' +
-    (isLocked ? ' disabled' : '') + '>' +
-    (Type === "bool" ? 'toggle' : 'save') + '</button>\n' +
-    '</li>\n' +
-    '<li class="pref-table__cell">\n' +
-    '<button class="button--ghost icon ' +
-    (isUser
-        ? 'icon--trash" title="delete preference"'
-        : 'icon--reset" ' + (isModified ? 'title="reset value to default"' : '')
-    ) +
-    (isLocked || (!isModified) ? ' disabled' : '') +
-    '></button> </li>\n';
+  function getFirstCol(row) {
+    var cell = document.createElement("li");
+    cell.className = "pref-table__cell";
+    var button = document.createElement("button");
+    button.classList.add("button", "button--ghost", "icon");
+    if (isLocked) {
+      button.classList.add("icon--lock");
+      button.title = "locked";
+      button.disabled = true;
+    } else if (isFav) {
+      button.classList.add("icon--filled-star");
+      button.title = "is favourite";
+      button.addEventListener("click", dummy);
+    } else {
+      button.classList.add("icon--star");
+      button.title = "make favourite";
+      button.addEventListener("click", dummy);
+    }
+    cell.appendChild(button);
+    row.appendChild(cell);
+  }
+
+  function getSecondCol(row) {
+    var cell = document.createElement("li");
+    cell.className = "pref-table__cell";
+    var text = document.createElement("span");
+    if (isLocked) {
+      text.className = "pref-table__cell__disabled-text";
+    }
+    var parts = name.split(".");
+    for (var i = 0; i < parts.length - 1; i++) {
+      text.append(parts[i] + ".");
+      text.append(document.createElement("wbr"));
+    }
+    text.append(parts[parts.length - 1]);
+    cell.appendChild(text);
+    row.appendChild(cell);
+  }
+
+  function getThirdCol(row) {
+    var cell = document.createElement("li");
+    cell.className = "pref-table__cell";
+    var inner;
+    if (type === "Boolean" || isLocked) {
+      inner = document.createElement("span");
+      inner.innerText = value;
+      if (isLocked) {
+        inner.className = "pref-table__cell__disabled-text";
+      }
+    } else {
+      inner = document.createElement("form");
+      inner.id = formName;
+      inner.setAttribute("aria-label", "Edit Value");
+      inner.addEventListener("submit", function (ev) {
+        ev.preventDefault();
+      });
+      var field = document.createElement("input");
+      field.type = "text";
+      field.value = value;
+      if (type === "Number") {
+        field.setAttribute("pattern", "-?[0-9]*");
+        field.title = "Please enter an integer value";
+      } else {
+        field.title = "Please enter the value";
+      }
+      inner.appendChild(field);
+    }
+
+    cell.appendChild(inner);
+    row.appendChild(cell);
+  }
+
+
+  function getFourthCol(row) {
+    var cell = document.createElement("li");
+    cell.className = "pref-table__cell";
+    var button = document.createElement("button");
+    button.classList.add("button", "button--small");
+    button.setAttribute("form", formName);
+    if (isLocked) {
+      button.disabled = true;
+    }
+    if (type === "Boolean") {
+      button.innerText = "toggle";
+      button.addEventListener("click", dummy);
+    } else {
+      button.innerText = "save";
+      button.addEventListener("click", dummy);
+    }
+
+    cell.appendChild(button);
+    row.appendChild(cell);
+  }
+
+  function getFifthCol(row) {
+    var cell = document.createElement("li");
+    cell.className = "pref-table__cell";
+    var button = document.createElement("button");
+    button.classList.add("button", "button--ghost", "icon");
+    if (isLocked || !isModified) {
+      button.disabled = true;
+    }
+    if (isUser) {
+      button.classList.add("icon--trash");
+      button.title = "delete preference";
+      button.addEventListener("click", dummy);
+    } else {
+      button.classList.add("icon--reset");
+      button.title = "reset value to default";
+      button.addEventListener("click", dummy);
+    }
+    cell.appendChild(button);
+    row.appendChild(cell);
+  }
+
+
+  var row = document.createElement("ul");
+  row.className = "pref-table__row";
+  if (isModified) {
+    row.classList.add("pref-table__row--edited");
+  }
+  row.setAttribute("aria-label", "Preference " + name);
+  row.setAttribute("role", "list ");
+
+  getFirstCol(row);
+  getSecondCol(row);
+  getThirdCol(row);
+  getFourthCol(row);
+  getFifthCol(row);
+
+  document.getElementById("pref-table").appendChild(row);
 }
 
-addItem("captivedetect.canonicalContent", "success", true, false, 'string', false, false);
-addItem("captivedetect.canonicalURL", "http://detectportal.firefox.com/success.txt", false, true, 'string', false, false);
-addItem("captivedetect.maxRetryCount", 5, true, true, 'int', true, false);
-addItem("captivedetect.maxWaitingTime", 5000, false, false, 'int', false, false);
-addItem("captivedetect.pollingTime", 3000, false, true, 'int', false, false);
-addItem("clipboard.autocopy", false, false, false, 'bool', true, false);
-addItem("clipboard.plainTextOnly", false, false, true, 'bool', false, true);
-addItem('more.accessibility.AOM.enabled', false, false, true, 'bool', true, true);
-addItem('more.accessibility.accesskeycausesactivation', true, false, false, 'bool', false, false);
-addItem('more.accessibility.blockautorefresh', false, false, true, 'bool', false, false);
-addItem('more.accessibility.browsewithcaret', false, false, false, 'bool', false, false);
-addItem('more.accessibility.browsewithcaret_shortcut.enabled', true, false, true, 'bool', false, false);
-addItem('more.accessibility.delay_plugin_time', 10000, true, true, 'int', false, true);
-addItem('more.accessibility.delay_plugins', false, false, true, 'bool', true, false);
-addItem('more.accessibility.force_disabled', 0, false, true, 'int', false, false);
-addItem('more.accessibility.handler.enabled', true, false, true, 'bool', false, true);
-addItem('more.accessibility.indicator.enabled', false, true, false, 'bool', false, false);
-addItem('more.accessibility.monoaudio.enable', false, false, true, 'bool', false, false);
-addItem('more.accessibility.mouse_focuses_formcontrol', false, false, true, 'bool', true, true);
-addItem('more.accessibility.support.url', 'https://support.mozilla.org/%LOCALE%/kb/accessibility-services', false, true, 'String', false, false);
-addItem('more.accessibility.tabfocus', 7, false, false, 'int', false, false);
-addItem('more.accessibility.tabfocus_applies_to_xul', false, false, false, 'bool', false, false);
-addItem('more.accessibility.typeaheadfind', false, false, true, 'bool', false, true);
-addItem('more.accessibility.typeaheadfind.autostart', true, false, false, 'bool', false, false);
-addItem('more.accessibility.typeaheadfind.casesensitive', 0, false, false, 'int', true, false);
-addItem('more.accessibility.typeaheadfind.enablesound', true, false, true, 'bool', false, false);
-addItem('more.accessibility.typeaheadfind.flashBar', 1, true, true, 'int', false, true);
-addItem('more.accessibility.typeaheadfind.linksonly', false, false, false, 'bool', false, false);
-addItem('more.accessibility.typeaheadfind.manual', true, false, true, 'bool', false, false);
+addItem("captivedetect.canonicalContent", "success", true, false, "string", false, false);
+addItem("captivedetect.canonicalURL", "http://detectportal.firefox.com/success.txt", false, true, "string", false, false);
+addItem("captivedetect.maxRetryCount", 5, true, true, "int", true, false);
+addItem("captivedetect.maxWaitingTime", 5000, false, false, "int", false, false);
+addItem("captivedetect.pollingTime", 3000, false, true, "int", false, false);
+addItem("clipboard.autocopy", false, false, false, "bool", true, false);
+addItem("clipboard.plainTextOnly", false, false, true, "bool", false, true);
+addItem("more.accessibility.AOM.enabled", false, false, true, "bool", true, true);
+addItem("more.accessibility.accesskeycausesactivation", true, false, false, "bool", false, false);
+addItem("more.accessibility.blockautorefresh", false, false, true, "bool", false, false);
+addItem("more.accessibility.browsewithcaret", false, false, false, "bool", false, false);
+addItem("more.accessibility.browsewithcaret_shortcut.enabled", true, false, true, "bool", false, false);
+addItem("more.accessibility.delay_plugin_time", 10000, true, true, "int", false, true);
+addItem("more.accessibility.delay_plugins", false, false, true, "bool", true, false);
+addItem("more.accessibility.force_disabled", 0, false, true, "int", false, false);
+addItem("more.accessibility.handler.enabled", true, false, true, "bool", false, true);
+addItem("more.accessibility.indicator.enabled", false, true, false, "bool", false, false);
+addItem("more.accessibility.monoaudio.enable", false, false, true, "bool", false, false);
+addItem("more.accessibility.mouse_focuses_formcontrol", false, false, true, "bool", true, true);
+addItem("more.accessibility.support.url", "https://support.mozilla.org/%LOCALE%/kb/accessibility-services", false, true, "String", false, false);
+addItem("more.accessibility.tabfocus", 7, false, false, "int", false, false);
+addItem("more.accessibility.tabfocus_applies_to_xul", false, false, false, "bool", false, false);
+addItem("more.accessibility.typeaheadfind", false, false, true, "bool", false, true);
+addItem("more.accessibility.typeaheadfind.autostart", true, false, false, "bool", false, false);
+addItem("more.accessibility.typeaheadfind.casesensitive", 0, false, false, "int", true, false);
+addItem("more.accessibility.typeaheadfind.enablesound", true, false, true, "bool", false, false);
+addItem("more.accessibility.typeaheadfind.flashBar", 1, true, true, "int", false, true);
+addItem("more.accessibility.typeaheadfind.linksonly", false, false, false, "bool", false, false);
+addItem("more.accessibility.typeaheadfind.manual", true, false, true, "bool", false, false);
 
 /*
 // get a list of all components
